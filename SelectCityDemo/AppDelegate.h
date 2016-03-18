f4d19365d4503492a8c909b39f411cf7355485e9
onzing <406036956@qq.com>
2016-03-18 09:31:06 +0800

    Initial Commit

---
+++ SelectCityDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectCityDemo
+//
+//  Created by 张勇杰 on 16/3/18.
+//  Copyright © 2016年 ZYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectCityDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectCityDemo
+//
+//  Created by 张勇杰 on 16/3/18.
+//  Copyright © 2016年 ZYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
